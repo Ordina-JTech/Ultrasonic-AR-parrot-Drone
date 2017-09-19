6c598682c6fd6db01eb594057a30ecf6bf676008
Evert <evert.poots@hccnet.nl>
2017-09-19 11:14:39 +0200

    Add files via upload

---
+++ sketch_sep18c/sketch_sep18c.ino
@@ -0,0 +1,46 @@
+// HC-SR04-test.ino
+// 
+// Get distance information from Ultrasonic Ranging Module HC-SR04 and 
+// send it via serial port.
+//
+// electronut.in
+
+#include "Arduino.h"
+
+int pinTrigger = 12;
+int pinEcho = 11;
+
+void setup()
+{
+  // initialize serial comms
+  Serial.begin(9600); 
+
+  // set pins 
+  pinMode(pinTrigger, OUTPUT);
+  pinMode(pinEcho, INPUT);
+}
+
+void loop()
+{
+  // send a 10us+ pulse
+  digitalWrite(pinTrigger, LOW);
+  delayMicroseconds(20);
+  digitalWrite(pinTrigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinTrigger, LOW);
+  delayMicroseconds(20);
+  
+  //  read duration of echo 
+  int duration = pulseIn(pinEcho, HIGH);
+
+  if(duration > 0) {
+    // dist = duration * speed of sound * 1/2
+    // dist in cm = duration in us * 1 x 10^{-6} * 340.26 * 100 * 1/2
+    // =  0.017*duration
+    float dist = 0.017 * duration;
+    Serial.println(dist);
+  }
+  
+  // wait 
+  delay(70);
+}
